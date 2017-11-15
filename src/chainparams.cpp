64725680437bb580bff9fe987a0e2131b036cce3
Remapper <33461506+Remapper@users.noreply.github.com>
2017-11-15 11:00:20 +0000

    Code update

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -492,6 +492,7 @@ const int SCRYPT_SCRATCHPAD_SIZE = 131072 + 63;
         //strSporkKey = "04B433E6598390C992F4F022F20D3B4CBBE691652EE7C48243B81701CBDB7CC7D7BF0EE09E154E6FCBF2043D65AF4E9E97B89B5DBAF830D83B9B7F469A6C45A717";
         strObfuscationPoolDummyAddress = "D87q2gC9j6nNrnzCsg4aY6bHMLsT9nUhEw";
         nStartMasternodePayments = 1403728576; //Wed, 25 Jun 2014 20:36:16 GMT
+        nBudget_Fee_Confirmations = 3; // Number of confirmations for the finalization fee
     }
 
     const Checkpoints::CCheckpointData& Checkpoints() const
